35e03f837bbfaab56cba6823685ec55a999d4f7e
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-05-19 10:35:56 -0400

    Removed redundant conversion methods.

--- src/core/types/signature.go
+++ src/core/types/signature.go
@@ -1,7 +1,6 @@
 package types
 
 import (
-	"encoding/hex"
 	"math/big"
 )
 
@@ -19,24 +18,4 @@ func BytesToSignature(b []byte) Signature {
 func BigToSignature(b *big.Int) Signature { return BytesToSignature(b.Bytes()) }
 
 // HexToSignature - Convert hex string to Signature
-func HexToSignature(s string) Signature { return BytesToSignature(fromHex(s)) }
-
-// fromHex - Generate byte array from hex string
-func fromHex(s string) []byte {
-	if len(s) > 1 {
-		if s[0:2] == "0x" || s[0:2] == "0X" {
-			s = s[2:]
-		}
-	}
-	if len(s)%2 == 1 {
-		s = "0" + s
-	}
-	return hex2Bytes(s)
-}
-
-// hex2Bytes - convert Hex string to byte array
-func hex2Bytes(str string) []byte {
-	h, _ := hex.DecodeString(str)
-
-	return h
-}
+func HexToSignature(s string) Signature { return BytesToSignature(FromHex(s)) }
