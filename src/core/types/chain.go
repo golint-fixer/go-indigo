cc9a6eb49d580ee63f6bc586d827c815269b4d85
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-05-26 09:56:42 -0400

    Added method for identifying unverified transactions.

--- src/core/types/chain.go
+++ src/core/types/chain.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"indogo/src/contracts"
+	"reflect"
 )
 
 // Chain - Connected collection of transactions
@@ -12,6 +13,27 @@ type Chain struct {
 	Transactions []*Transaction
 }
 
-func AddTransaction(Transaction *Transaction) {
+// AddTransaction - Add transaction to specified chain object
+func AddTransaction(Transaction *Transaction, RefChain Chain) {
+	RefChain.Transactions = append(RefChain.Transactions, Transaction)
+}
 
+// FindUnverifiedTransactions - Browse chain for most recent unverified transactions
+func FindUnverifiedTransactions(RefChain Chain, TxCount int) []*Transaction {
+
+	var UnverifiedTransactions []*Transaction
+
+	x := len(RefChain.Transactions) - 1
+
+	targetTxCount := TxCount + 1
+
+	for len(UnverifiedTransactions) != targetTxCount {
+		if reflect.ValueOf(RefChain.Transactions[x].InitialWitness).IsNil() || reflect.ValueOf(RefChain.Transactions[x].InitialWitness).IsNil() {
+			UnverifiedTransactions = append(UnverifiedTransactions, RefChain.Transactions[x])
+		} else {
+			x--
+		}
+	}
+
+	return UnverifiedTransactions
 }
