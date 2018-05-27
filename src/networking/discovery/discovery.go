06525ef08f3dff913b27a560d3578a2eb3700ec7
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-05-26 20:35:22 -0400

    Backup.

--- src/networking/discovery/discovery.go
+++ src/networking/discovery/discovery.go
@@ -1,1 +1,12 @@
 package discovery
+
+import "indogo/src/networking"
+
+type nodeDatabase struct {
+	lvl     *leveldb.DB
+	selfRef networking.NodeID
+}
+
+func newNodeDatabase(selfRef networking.NodeID) *nodeDatabase {
+
+}
