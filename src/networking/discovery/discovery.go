a9ca5fbf90d1190e0a3a007b19e95ca32625c70f
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-05-27 09:58:57 -0400

    Backup.

--- src/networking/discovery/discovery.go
+++ src/networking/discovery/discovery.go
@@ -1,12 +1,49 @@
 package discovery
 
-import "indogo/src/networking"
+import (
+	"fmt"
+	"indogo/src/networking"
+	"indogo/src/networking/fastping"
+	"net"
+	"os"
+	"time"
+)
 
-type nodeDatabase struct {
-	lvl     *leveldb.DB
-	selfRef networking.NodeID
+// NodeDatabase - struct holding arrays of IP addresses, node IDs, etc...
+type NodeDatabase struct {
+	NodeRefDB      []networking.NodeID
+	NodePingDB     []int
+	NodePingTimeDB []time.Time
+	NodeAddress    []string
+	SelfRef        networking.NodeID
 }
 
-func newNodeDatabase(selfRef networking.NodeID) *nodeDatabase {
+// NewNodeDatabase - return new node database initialized with self ID
+func NewNodeDatabase(selfRef networking.NodeID) *nodeDatabase {
+	return &nodeDatabase{SelfRef: selfRef}
+}
 
+func (db *nodeDatabase) addNode(ip string, id networking.NodeID) {
+	p := fastping.NewPinger()
+	ra, err := net.ResolveIPAddr(ip, os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		fmt.Println("Node tested successfully: " + ip)
+	}
+}
+
+func (db *nodeDatabase) lastPing(id networking.NodeID) time.Time {
+	nodeIndex := db.getNodeIndex(id)
+	return db.NodePingTimeDB[nodeIndex]
+}
+
+func (db *nodeDatabase) getNodeIndex(id networking.NodeID) int {
+	for k, v := range db.NodeRefDB {
+		if id == v {
+			return k
+		}
+	}
+	return 0
 }
