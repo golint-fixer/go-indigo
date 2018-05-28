bcc1ab77637173de7b286ad775510f2e4f3f37f8
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-05-27 20:55:51 -0400

    Refactored to minimize online dependencies.

--- src/networking/fastping/fastping.go
+++ src/networking/fastping/fastping.go
@@ -49,9 +49,9 @@ import (
 )
 
 import (
-	"golang.org/x/net/icmp"
-	"golang.org/x/net/ipv4"
-	"golang.org/x/net/ipv6"
+	"indogo/src/golang.org/x/net/icmp"
+	"indogo/src/golang.org/x/net/ipv4"
+	"indogo/src/golang.org/x/net/ipv6"
 )
 
 const (
